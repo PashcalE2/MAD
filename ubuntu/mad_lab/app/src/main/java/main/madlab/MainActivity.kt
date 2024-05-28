7c80e5cdf2371128edb862867b51c71b7b650bc5
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-05-28 22:39:01 +0300

    .

--- ubuntu/mad_lab/app/src/main/java/main/madlab/MainActivity.kt
+++ ubuntu/mad_lab/app/src/main/java/main/madlab/MainActivity.kt
@@ -1,12 +1,23 @@
 package main.madlab
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var devicesLayout: LinearLayout
+    private var devicesList = mutableListOf<View>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        devicesLayout = findViewById(R.id.devices_layout)
+
+        button = findViewById(R.id.add_button)
+        button.setOnClickListener { _ ->
+            val newDeviceLayout: ViewGroup = FrameLayout(devicesLayout.context)
+            val newDevice: View = LayoutInflater.from(newDeviceLayout.context).inflate(R.layout.device, newDeviceLayout, true)
+
+            devicesList.add(newDeviceLayout)
+            devicesLayout.addView(newDeviceLayout)
+
+            println(devicesLayout.childCount)
         }
     }
 }
